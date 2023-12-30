7c2c660840c6cd4ad7db13f57d41ea92b4728b24
anurag yadav <anurag5713755@gmail.com>
2023-12-31 02:12:40 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 3000, // Set a higher value based on your needs
+    chunkSizeWarningLimit: 3500, // Set a higher value of chunk
   },
-  plugins: [react()],
 })
