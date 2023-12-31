4ff1676d78c707df0fef867509c4037fcaf0f61b
anurag yadav <anurag5713755@gmail.com>
2023-12-31 21:32:09 +0530

    reltive path added in verify email

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
-  // build: {
-  //   chunkSizeWarningLimit: 3500, // Set a higher value of chunk
-  // },
+   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   build: {
     outDir:"dist",
     chunkSizeWarningLimit: 3000,
